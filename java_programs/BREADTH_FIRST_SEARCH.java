149f09a84d9eb620fd97cd1afe20bbe43afa2baf
Hangzhi <hangzhiweiwei@gmail.com>
2025-09-03 15:13:56 +0000

    java programs

--- java_programs/BREADTH_FIRST_SEARCH.java
+++ java_programs/BREADTH_FIRST_SEARCH.java
@@ -39,7 +39,7 @@ public class BREADTH_FIRST_SEARCH {
          * The buggy program always drops into while(true) loop and will not return false
          * Removed below line to fix compilation error
          */
-        return false;
+        // return false;
     }
 
 }
